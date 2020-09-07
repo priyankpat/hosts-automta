2894c0973d4f82b5c7a52967e5d7da3cef1d63b9
Priyank Patel <priyank.patel@stackspace.ca>
2020-09-07 13:56:52 -0400

    Update unix links

--- unix/install.sh
+++ unix/install.sh
@@ -13,6 +13,6 @@ fi
 sudo mv /etc/hosts /etc/hosts.bak
 
 # Now download the new hosts file and put it into place
-sudo wget https://gist.githubusercontent.com/priyankpat/68a6f96ed1e3fbe974fe1f573248b1ff/raw/f5303543cbed28421bfcfcbd0451e59fb0441eb2/gistfile1.txt -O /etc/hosts
+sudo wget https://gist.githubusercontent.com/priyankpat/68a6f96ed1e3fbe974fe1f573248b1ff/raw/5db9ed7a2bc80220d882c3e73f2a8714498c5b02/gistfile1.txt -O /etc/hosts
 
 exit 0
